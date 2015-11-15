24422541d78dfc006fa8b402f142dfa531dc2ec9
AnkilP <ankil335@gmail.com>
2015-11-15 02:04:15 -0500

    sorry

--- public/js/student.js
+++ public/js/student.js
@@ -1,6 +1,6 @@
 (function() {
 	angular.module('tutorApp', ["ngAnimate"]).controller('studentCtrl', ['$scope',
-	function($scope, modals) {
+	function($scope) {
     $scope.subject = {
         items: [{
             qty: 2,
@@ -100,4 +100,39 @@
         $scope.subject.items.splice(index, 1);
     }
 	}]);
+})();
+(function() {
+    angular.module('tutorApp'.controller("AlertModalController", function($scope,modals){
+        $scope.message = ( modals.params().message || "Whoa!" );
+                // ---
+                // PUBLIC METHODS.
+                // ---
+                // Wire the modal buttons into modal resolution actions.
+                $scope.close = modals.resolve;
+                // I jump from the current alert-modal to the confirm-modal.
+                $scope.jumpToConfirm = function() {
+                    // We could have used the .open() method to jump from one modal
+                    // to the next; however, that would have implicitly "rejected" the
+                    // current modal. By using .proceedTo(), we open the next window, but
+                    // defer the resolution of the current modal until the subsequent
+                    // modal is resolved or rejected.
+                    modals.proceedTo(
+                        "confirm",
+                        {
+                            message: "You selected" + tutorList.name,
+                            confirmButton: "I want him to be your mentor",
+                            denyButton: "No, I don't like his hat"
+                        }
+                    )
+                    .then(
+                        function handleResolve() {
+                            console.log( "Piped confirm resolved." );
+                        },
+                        function handleReject() {
+                            console.warn( "Piped confirm rejected." );
+                        }
+                    );
+                };
+            }
+        ))
 })();
