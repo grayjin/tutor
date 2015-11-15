f0e822e1634b40c90c152addd448beed009a1b6f
AnkilP <ankil335@gmail.com>
2015-11-14 20:31:07 -0500

    course selection

--- public/js/student.js
+++ public/js/student.js
@@ -1,7 +1,25 @@
 (function() {
 	angular.module('tutorApp').controller('studentCtrl', ['$scope',
 	function($scope) {
-		t
+    $scope.subject = {
+        items: [{
+            qty: 2,
+            course_code: ""
+        }]
+    };
+    $scope.submit = function() {
+        if ($scope.text) {
+          $scope.table.push(this.text);
+          $scope.text = '';
+        }
+    };
+    $scope.addItem = function () {
+        $scope.subject.items.push({
+            qty: 1,
+        });
+    },
+    $scope.removeItem = function (index) {
+        $scope.subject.items.splice(index, 1);
+    }
 	}]);
-
 })();
