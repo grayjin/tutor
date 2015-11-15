98380c3c80635fbe38a8c1ec7f57f28ed7af29cb
AnkilP <ankil335@gmail.com>
2015-11-14 21:37:58 -0500

    added students

--- public/pages/CourseSelection.js
+++ public/pages/CourseSelection.js
@@ -1,5 +1,33 @@
 angular.module('tutorApp').directive('controlcourse', function() {
 	return {
+		template: <div>
+					<h2> Course Selection </h2>
+					<form ng-submit="submit()" method = "post" action = "">
+				    <table class="table">
+				        <tr>
+				            <th>Description</th>
+				            <th>Course Code</th>
+				            <th>Remove Course</th>
+				        </tr>
+				        <tr></tr>
+				        <tr ng:repeat="item in subject.items">
+				            <td>
+				                <input type="text" name:"subject" </input>
+				            </td>
+				            <td>
+				            	<input type="text" name:"course_ code"</input>
+				            </td>
+				            <td>[<a href ng:click="removeItem($index)">X</a>]</td>
+				        </tr>
+				        <tr>
+				            <td><a href ng:click="addItem()" class="btn btn-small">add Course</a>
+
+				            </td>
+				        </tr>
+				    </table>
+					  <input type="submit" id="submit" value="Submit" />
+					</form>
+				</div>
         templateUrl: 'CourseSelection.html',
         controller: (function() {
 				angular.module('tutorApp').controller('studentCtrl', ['$scope',
