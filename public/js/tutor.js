85c2734f570284b99a9c5a06a02f0905f2c10411
Gray Jin <grayjin@v1020-wn-96-88.campus-dynamic.uwaterloo.ca>
2015-11-14 21:10:42 -0500

    added tutor scheduler

--- public/js/tutor.js
+++ public/js/tutor.js
@@ -1,6 +1,48 @@
 (function() {
 	angular.module('tutorApp').controller('tutorCtrl', ['$scope',
 	function($scope) {
+		var vm = this;
+
+		$scope.scheduleList = [];
+
+		$scope.initializeList = function() {
+			console.log("initializeList");
+			for(var i = 0; i < 24; i++) {
+				var dayArray = [];
+
+				dayArray.push(i+":00");
+				for(var j = 0; j < 7; j++) {
+					dayArray.push(0);
+				}
+
+				$scope.scheduleList.push(dayArray);
+			}
+			console.log($scope.scheduleList);
+		}
+
+		$scope.getScheduleType = function(type) {
+			if (type === 1){
+				return "selectedBlock";
+			} else if (type === 0 ){
+				return "unselectedBlock";
+			} else {
+				return "timeLabel"
+			}
+		}
+
+		$scope.toggleSection = function (date, time){
+			if (time != 0){
+				$scope.scheduleList[date][time] = ($scope.scheduleList[date][time] + 1) % 2;
+			}
+		}
+
+		$scope.submitSchedule = function() {
+			for (var i = 0; i < 24; i++) {
+				for (var j = 1; j < 8; j++) {
+					console.log($scope.scheduleList[i][j]);
+				}
+			}
+		}
 
 	}]);
 
