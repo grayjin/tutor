97de5615e2e9932255aa22932f2fc66b5403f53b
calderk2010 <calderk2010@gmail.com>
2015-11-15 07:24:20 -0500

    Tabs

--- public/js/profile.js
+++ public/js/profile.js
@@ -23,7 +23,7 @@
 		$scope.initializeProfile = function(){
 			$scope.profileSummary = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 			$scope.profileLocation = 1;
-		}	
+		}
 
 		$scope.editMode = function(){
 			$scope.tempSummary = $scope.profileSummary;
@@ -44,6 +44,20 @@
 		$scope.getEditMode = function() {
 			return vm.editMode;
 		}
+
+    $scope.switch1 = "Student \n Calendar";
+    $scope.switch2 = "Tutor \n Calendar"
+    $scope.switch = $scope.switch1
+
+    $scope.viewToggle = function() {
+      if($scope.switch == $scope.switch1){
+        $scope.switch = $scope.switch2
+
+      } else if($scope.switch == $scope.switch2){
+        $scope.switch = $scope.switch1
+
+      }
+    }
 
 	}]);
 
