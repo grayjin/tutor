b8c065243a22e96b255c12989dfbe30ed72085c8
Ahmad <ahmad_h7@live.com>
2015-11-14 20:49:15 -0500

    tenth thousand query attempt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var http = require('http');
 
-var db = require('./js/dbOperations.js');
+var db = require('/js/dbOperations.js');
 
 app.use('/node_modules', express.static(__dirname + '/node_modules/'));
 
