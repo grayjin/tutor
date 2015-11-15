222652ad952c7412c3285aaf09821b8c55c6a27c
AnkilP <ankil335@gmail.com>
2015-11-15 02:05:47 -0500

    sdfasd

--- public/js/student.js
+++ public/js/student.js
@@ -101,38 +101,3 @@
     }
 	}]);
 })();
-(function() {
-    angular.module('tutorApp'.controller("AlertModalController", function($scope,modals){
-        $scope.message = ( modals.params().message || "Whoa!" );
-                // ---
-                // PUBLIC METHODS.
-                // ---
-                // Wire the modal buttons into modal resolution actions.
-                $scope.close = modals.resolve;
-                // I jump from the current alert-modal to the confirm-modal.
-                $scope.jumpToConfirm = function() {
-                    // We could have used the .open() method to jump from one modal
-                    // to the next; however, that would have implicitly "rejected" the
-                    // current modal. By using .proceedTo(), we open the next window, but
-                    // defer the resolution of the current modal until the subsequent
-                    // modal is resolved or rejected.
-                    modals.proceedTo(
-                        "confirm",
-                        {
-                            message: "You selected" + tutorList.name,
-                            confirmButton: "I want him to be your mentor",
-                            denyButton: "No, I don't like his hat"
-                        }
-                    )
-                    .then(
-                        function handleResolve() {
-                            console.log( "Piped confirm resolved." );
-                        },
-                        function handleReject() {
-                            console.warn( "Piped confirm rejected." );
-                        }
-                    );
-                };
-            }
-        ))
-})();
